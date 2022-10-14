711acc3fa2f353a4eefba1a67a0cc66f868841b0
Natallia Siniak <natalia.vasilevna.s@gmail.com>
2022-10-14 19:17:07 +0400

    feat: add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (Array.isArray(members) == false) return false; 
+
+  const arrayWihtoutWhiteSpaces = [];
+  for (let i = 0; i < members.length; i++) {
+    if (typeof(members[i]) === 'string')  {
+      //console.debug(members[i]);
+      arrayWihtoutWhiteSpaces.push(members[i].replace(/^\s+/g, '').toUpperCase());
+    }
+  }
+  
+  const membersSortAlf = arrayWihtoutWhiteSpaces.sort();
+  let dreamTeam = '';
+  for (let i = 0; i < membersSortAlf.length; i++) {
+    if (typeof((membersSortAlf[i]) === 'string') ) {
+      dreamTeam += membersSortAlf[i].charAt(0);
+    }
+
+  }
+  if (dreamTeam === '') return false;
+  //console.debug(`Dream ${dreamTeam} cheking ${cheking}`);
+  return dreamTeam;
 }
 
 module.exports = {
